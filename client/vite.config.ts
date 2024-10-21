d12197c38f0e2f8b37ca4921b242550e59804e35
Adam Robson <adamrayrobson@gmail.com>
2024-10-20 22:51:54 -0700

    add proxy to server and vitest config in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest/config" />
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+    environment: 'jsdom'
+  },
+  server: {
+    proxy: {
+      '/api/v1': {
+        target: 'http://localhost:3000'
+      }
+    }
+  }
+});
